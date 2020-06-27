720688dafd7ddab1f92b14314912516078f3fc0a
oxine <ooxinee@gmail.com>
2020-06-27 19:23:07 +0800

    bug fix
    · 关闭lighting 的auto generation
    · inspector优化
    · 跳跃特效不仅仅在第一次跳跃时出现

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,22 +18,27 @@ public class Player : MonoBehaviour
 
     private Collider m_lastCollisionCollider;
 
-    public GameObject Stage;
+    private Transform mainCamera;
 
-    public Transform Camera;
+    public GameObject Stage;
 
     public Transform Head;
 
     public Transform Body;
 
     public TextMeshProUGUI ScoreText;
+    
+    private GameObject Particle;
 
-    public GameObject Particle;
-
+    private Transform Stages;
 #endregion
 
 #region propertity
     private float _state_time;
+
+    [SerializeField]
+    [Rename("盒子塌缩速度")]
+    private float _stageCollapseSpeed=0.15f;
 
     public float Factor = 1;
 
@@ -42,14 +47,17 @@ public class Player : MonoBehaviour
     //相机的相对位置
     public Vector3 Camera_relative_position;
 
-    private Vector3 _direction = new Vector3(1,0,0);
+    private Vector3 _direction = Vector3.right;
 
     private int _score;
 
-    private Vector3 _currentStageScale;
-#endregion
+    private Vector3 _currentStageScale = new Vector3(1, 0.5f, 1);
+    #endregion
 
-
+    void Awake() {
+        mainCamera = Camera.main.transform;
+        Stages = new GameObject("Stages").transform;
+    }
     void Start()
     {
         m_rigidBody = GetComponent<Rigidbody>();
@@ -64,7 +72,7 @@ public class Player : MonoBehaviour
 
         SpawnStage();
 
-        Camera_relative_position = Camera.position - transform.position;
+        Camera_relative_position = mainCamera.position - transform.position;
 
         Particle = GameObject.Find("Particle System");
         Particle.SetActive(false);
@@ -95,14 +103,13 @@ public class Player : MonoBehaviour
         //按下空格键，小人身体进行缩放,盒子缩放--蓄力效果
         if(Input.GetKey(KeyCode.Space))
         {
-            float stageCollapseSpeed = 0.15f;
             //别把盒子塌缩没了
             if (m_currentStage.transform.localScale.y > 0.1f) {
-                Body.transform.localScale += Vector3.down * stageCollapseSpeed * Time.deltaTime*0.13f;
+                Body.transform.localScale += Vector3.down * _stageCollapseSpeed * Time.deltaTime*0.13f;
                 //Head.transform.localPosition += Vector3.down * 0.f * Time.deltaTime * 0.5f; Since you are using gravity, set the pos of player seems to be useless
                 //盒子沿着轴心缩放 如果你想让盒子严格沿底部为轴缩放，位移必须是缩放的一半。
-                m_currentStage.transform.localScale += Vector3.down * stageCollapseSpeed * Time.deltaTime;
-                m_currentStage.transform.localPosition += Vector3.down * stageCollapseSpeed * Time.deltaTime * 0.5f;
+                m_currentStage.transform.localScale += Vector3.down * _stageCollapseSpeed * Time.deltaTime;
+                m_currentStage.transform.localPosition += Vector3.down * _stageCollapseSpeed * Time.deltaTime * 0.5f;
             }
            
 
@@ -128,11 +135,13 @@ public class Player : MonoBehaviour
 
         stage.transform.localScale = new Vector3(random_scale, 0.5f, random_scale);
         stage.GetComponent<Renderer>().material.color = new Color(Random.Range(0.01f, 1), Random.Range(0.01f, 1), Random.Range(0.01f, 1));
-
+        stage.transform.parent = Stages;
+        stage.name = "Stage" + _score.ToString();
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        //I suppose that using name is a BAD idea, using tag or layer for instead
         if(collision.gameObject.name.Contains("Stage") && collision.collider != m_lastCollisionCollider)
         {
             m_lastCollisionCollider = collision.collider;
@@ -145,6 +154,8 @@ public class Player : MonoBehaviour
 
             _score++;
             ScoreText.text = _score.ToString();
+
+            Particle.transform.position = Body.transform.position;
         }
         if(collision.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
         {
@@ -165,7 +176,7 @@ public class Player : MonoBehaviour
     }
     void MoveCamera()
     {
-        Camera.DOMove(transform.position + Camera_relative_position, 1);
+        mainCamera.DOMove(transform.position + Camera_relative_position, 1);
     }
 
     void PredictTrajectory()
